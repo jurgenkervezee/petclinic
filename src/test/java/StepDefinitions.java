b7badedef65cf0bc802dcf48bec863fc5c0932bb
jurgenkervezee <jurgenbla@gmail.com>
2018-03-24 10:47:09 +0100

    feature file ingekopieerd template stepdefinitions gegenereerd

--- src/test/java/StepDefinitions.java
+++ src/test/java/StepDefinitions.java
@@ -1,4 +1,50 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class StepDefinitions {
 
+    @Given("^I am on the \"([^\"]*)\"$")
+    public void i_am_on_the(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see \"([^\"]*)\"$")
+    public void i_should_see(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I press \"([^\"]*)\"$")
+    public void i_press(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I follow \"([^\"]*)\"$")
+    public void i_follow(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I open nos\\.nl$")
+    public void i_open_nos_nl() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I launch nos\\.nl$")
+    public void i_launch_nos_nl() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I am at nos\\.nl$")
+    public void i_am_at_nos_nl() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 
 }
---
+++ src/test/smoketest.feature
@@ -0,0 +1,23 @@
+Feature: Browsing around
+
+  Scenario: The home page
+    Given I am on the "home page"
+    Then I should see "Welcome"
+
+  Scenario: Owner page
+    Given I am on the "owners search page"
+    And I press "Find Owner"
+    When I follow "George Franklin"
+    Then I should see "Pets and Visits"
+    And I should see "Leo"
+
+  Scenario: Test
+    Given I am on the "home page"
+    Given I am on the "owners search page"
+    Given I am on the "new owner page"
+
+
+  Scenario: Alternatieven voorbeeld
+    Given I open nos.nl
+    Given I launch nos.nl
+    Given I am at nos.nl
